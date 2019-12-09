fb70220f6ddf2c63b3b6b9b6e93b35c46c9ecc5b
Sergei <sergei.kozinecs@gmail.com>
2019-12-09 23:25:48 +0200

    Updates

---
+++ precache-manifest.92c67977da4b06e516ca6c21c14513ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54659f3eadaa59c92b560e1e1e801e4",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "3c4018e242a0496a4e24",
+    "url": "/mosters-rolodex/static/css/main.687b5dd3.chunk.css"
+  },
+  {
+    "revision": "ca805c5e01ac68d8fc4b",
+    "url": "/mosters-rolodex/static/js/2.8181dcb0.chunk.js"
+  },
+  {
+    "revision": "3c4018e242a0496a4e24",
+    "url": "/mosters-rolodex/static/js/main.b983889d.chunk.js"
+  },
+  {
+    "revision": "82ed77173ef3374ab3c4",
+    "url": "/mosters-rolodex/static/js/runtime-main.3828941b.js"
+  }
+]);
